759c85537d9446807ef271dcfa0067028957b483
fpanitz <Felix.Panitz@tu-dresden.de>
2022-09-12 07:34:35 +0200

    test

--- examples/Example_Full_Seg_Agg/Model_and_solve.py
+++ examples/Example_Full_Seg_Agg/Model_and_solve.py
@@ -23,10 +23,10 @@ nameSuffix = '_' + solver_name # for saving-file
 ## Auswahl Rechentypen: ##
 
 doFullCalc = True
-doFullCalc = False
+# doFullCalc = False
 
 doSegmentedCalc = True
-doSegmentedCalc = False
+# doSegmentedCalc = False
 
 doAggregatedCalc = True
 # doAggregatedCalc = False
@@ -334,6 +334,7 @@ for aResult in listOfCalcs:
   # print(aResult.duration)
   print('costs: ' + str(aResult.results_struct.globalComp.costs.all.sum))
 
+  
 
 #######################
 ### post processing ###
@@ -349,16 +350,34 @@ if doFullCalc:
     full = flixPost.flix_results(calcFull.nameOfCalc)
     listOfResults.append(full)
     del calcFull
-
+    
+    costs = full.results_struct.globalComp.costs.all.sum
+    if (np.round(costs) == np.round(361984.0791699171)):
+        print('################')
+        print('full: test is ok') 
+    else: 
+        raise Exception('test is not ok')
 if doAggregatedCalc:
     agg = flixPost.flix_results(calcAgg.nameOfCalc)
     listOfResults.append(agg)
     del calcAgg
+    costs = agg.results_struct.globalComp.costs.all.sum
+    if (np.round(costs) == np.round(359613.8834453795)):
+        print('################')
+        print('agg: test is ok')
+    else:
+        raise Exception('test is not ok')
 
 if doSegmentedCalc:
     seg = flixPost.flix_results(calcSegs.nameOfCalc)
     listOfResults.append(seg)
     del calcSegs
+    costs = seg.results_struct.globalComp.costs.all.sum
+    if all (np.round(costs) == np.round([210654.98636908, 269804.58925733])):
+        print('################')
+        print('seg: test is ok')    
+    else:
+        raise Exception('test is not ok')        
 
 ###### plotting #######
 
